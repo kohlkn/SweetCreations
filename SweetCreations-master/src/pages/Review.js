ebfb270e00916704c000d2ff3a3f1789280fb805
unknown <jennyngo1925@gmail.com>
2022-11-17 15:24:39 -0500

    fixed form again lol

--- SweetCreations-master/src/pages/Review.js
+++ SweetCreations-master/src/pages/Review.js
@@ -3,46 +3,53 @@ import { useState } from "react";
 import ReactDOM from "react-dom/client";
 
 export default function Review() {
-  const [name, setName] = useState("");
+
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(`The name you entered was: ${name}`);
-  };
+    console.log(inputs);
+  }
 
-    const [textarea, setTextarea] = useState(
-    "Leave a review..."
+  const [textarea, setTextarea] = useState(
+    "The content of a textarea goes in the value attribute"
   );
 
-  const handleChange = (event) => {
-    setTextarea(event.target.value)
-  }
 
   return (
     <form onSubmit={handleSubmit}>
-      <br></br>
-      <br></br>
-      <h1>Leave A Review!</h1>
-      <label>
-        Name:
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+    <br></br>
+    <br></br>
+    <h1>Leave A Review!</h1>
+      <label>Name:
+      <input 
+        type="text" 
+        name="name" 
+        value={inputs.name || ""} 
+        onChange={handleChange}
+      />
+          <br></br>
+    <br></br>
       </label>
-      <br></br>
-      <br></br>
-      <label>
-        Review:<br></br>
-        <textarea rows="4" cols="50" value={textarea} onChange={handleChange} />
-      </label>
-      <br></br>
-      <br></br>
-      <input type="submit" />
-    </form>
-  );
-}
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Review />);
+      <label>
+         Review:<br></br>
+         <textarea rows="4" cols="50" 
+          type="text" 
+          name="review" 
+          value={inputs.review || ""}
+          onChange={handleChange}
+        />      
+            <br></br>
+    <br></br>
+       </label>
+        <input type="submit" />
+    </form>
+  )
+}
